0e6157f0d32626689da6b3b99911023c234785f8
Antonio9026 <121766004+Antonio9026@users.noreply.github.com>
2023-09-26 17:47:50 +0200

    card stampate no stile

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -2,5 +2,18 @@
 
 @section("content")
 
-<h1>movie</h1>
+
+<div class="container">
+@foreach ($movies as $movie )
+<div class="card">
+<h2>{{$movie->title}}</h2>
+<h3>{{$movie->original_title}}</h3>
+<p>{{$movie->nationality}}</p> 
+<p>{{$movie->date}}</p>
+<p>{{$movie->vote}}</p>
+</div>
+@endforeach 
+  
+
+</div>
 @endsection
