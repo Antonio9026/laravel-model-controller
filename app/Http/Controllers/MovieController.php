0481df0bd372c9ec54d492d4a1c7a5279c89e2f1
Antonio9026 <121766004+Antonio9026@users.noreply.github.com>
2023-09-26 16:42:17 +0200

    errore mvie controller

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -4,10 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class MovieController extends Controller
-{
-    public function index (){
-
-         return view('movie');
+ class MovieController extends Controller{
+    public function index(){
+        return view('movie');
     }
 }
---
+++ resources/views/movie.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.public')
+
+@section("content")
+
+<h1>movie</h1>
+@endsection
