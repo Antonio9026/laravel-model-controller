abeb27d4e9060d95dbb465be3e8698eecf1cd26b
Antonio9026 <121766004+Antonio9026@users.noreply.github.com>
2023-09-26 17:40:22 +0200

    database importato

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
- class MovieController extends Controller{
-    public function index(){
-        return view('movie');
+class MovieController extends Controller
+{
+    public function index()
+    {
+        $movies =  Movie::all();
+        // dd($movies);
+
+        return view('movie',["movies"=>$movies]);
     }
 }
